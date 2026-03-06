0ff7e4b360ac8a8c94780763e1d3da3403726981
Somyansh <kaixzer1@gmail.com>
2026-03-06 17:58:19 +0530

    Frontend made and connected to the Flash API by Preksha

---
+++ frontend/script.js
@@ -0,0 +1,32 @@
+async function analyze(){
+
+    const message = document.getElementById("message").value;
+
+    const response = await fetch("http://127.0.0.1:5000/predict",{
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            text: message
+        })
+
+    });
+
+    const data = await response.json();
+
+    const resultDiv = document.getElementById("result");
+
+if(data.prediction === "toxic"){
+    resultDiv.innerHTML = "🔴 Toxic Message";
+    resultDiv.style.color = "red";
+}
+else{
+    resultDiv.innerHTML = "🟢 Clean Message";
+    resultDiv.style.color = "lightgreen";
+}
+
+}
