d06dc85d757590b91e2afb881c1851ba6b438163
Somyansh <kaixzer1@gmail.com>
2026-03-08 22:00:00 +0530

    Updated frontend UI with lamp indicator for toxicity detection

--- frontend/script.js
+++ frontend/script.js
@@ -19,14 +19,26 @@ async function analyze(){
     const data = await response.json();
 
     const resultDiv = document.getElementById("result");
+    const lamp = document.getElementById("lamp");
+
 
 if(data.prediction === "toxic"){
+
     resultDiv.innerHTML = "🔴 Toxic Message";
     resultDiv.style.color = "red";
+
+    lamp.classList.remove("clean");
+    lamp.classList.add("toxic");
+
 }
 else{
+
     resultDiv.innerHTML = "🟢 Clean Message";
     resultDiv.style.color = "lightgreen";
+
+    lamp.classList.remove("toxic");
+    lamp.classList.add("clean");
+
 }
 
 }
