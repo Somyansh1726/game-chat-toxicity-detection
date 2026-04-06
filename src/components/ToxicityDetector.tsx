2f8bf79706a7fe096cc25f8d70368aa55a46a43b
Somyansh <kaixzer1@gmail.com>
2026-04-06 21:55:21 +0530

    fixed api url

--- src/components/ToxicityDetector.tsx
+++ src/components/ToxicityDetector.tsx
@@ -19,7 +19,7 @@ const ToxicityDetector = ({ onResult }: ToxicityDetectorProps) => {
     setIsAnalyzing(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://game-chat-toxicity-detection.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
